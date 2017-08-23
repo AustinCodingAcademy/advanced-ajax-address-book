206b2ed76f87e6c645512e09a531793a444dfb5b
Mehmet Sonmezates <sonmezatesmehmet@gmail.com>
2017-08-23 14:12:17 -0500

    implement thunk to use fetch with redux

--- src/App.js
+++ src/App.js
@@ -4,15 +4,40 @@ import "./App.css";
 import UserDetailContainer from "./containers/UserDetailContainer";
 import ListOfUsersContainer from "./containers/ListOfUsersContainer";
 import SearchBox from "./components/SearchBox";
+import CreateUser from "./components/CreateUser";
+import { loadUsers } from "./actions";
+import { connect } from "react-redux";
 
-function App(props) {
-  return (
-    <div>
-      <SearchBox />
-      <ListOfUsersContainer />
-      <UserDetailContainer />
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      users: []
+    };
+  }
+
+  componentDidMount() {
+    this.props.loadData();
+  }
+
+  render() {
+    return (
+      <div>
+        <SearchBox />
+        <ListOfUsersContainer />
+        <CreateUser />
+        <UserDetailContainer />
+      </div>
+    );
+  }
 }
 
-export default App;
+function mapDispatchToProps(dispatch) {
+  return {
+    loadData() {
+      dispatch(loadUsers());
+    }
+  };
+}
+
+export default connect(null, mapDispatchToProps)(App);
